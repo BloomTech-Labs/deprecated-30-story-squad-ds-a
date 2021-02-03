9fb29e81d36f82961d009910d0534fa60791b30d
Eli Fulton <elifulton@brights-mbp.localdomain>
2021-02-03 09:08:40 -0800

    Added extra message detailing where to find neural network training data

--- illustration_api.py
+++ illustration_api.py
@@ -12,8 +12,9 @@ app = FastAPI(
 
 # this api is meant to help transfer the illustration similarity scoring model
 # when set up properly, there should be a file called 'transfer_model.h5' in the same scope as this illustration_api file
-# to do this, follow this link: https://colab.research.google.com/drive/1J66ylaqZfZQzCiOmRYHJ4mWt7Jmh7y_B?usp=sharing
-# run all the cells properly, and take the newly downloaded h5 from your Colaboratory workflow to the story-squad-ds-a main folder
+# first, you'll want to download the data that the neural network will use, which can be found at this link: https://drive.google.com/drive/folders/1rWbjhPRoGj-kwvESVUWhAigfecsN6XDo?usp=sharing
+# then open the Google Colaboratory notebook that can be found here: https://colab.research.google.com/drive/1J66ylaqZfZQzCiOmRYHJ4mWt7Jmh7y_B?usp=sharing
+# get the data folder to the "Files" sidebar, run all the cells properly, and take the newly downloaded h5 from your Colaboratory workflow to the story-squad-ds-a main folder
 # lastly, uncomment the line below this one
 
 #model = load_model('transfer_model.h5')
