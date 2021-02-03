1098a133b787bec13ec484035947a36014dbda9c
Eli Fulton <elifulton@brights-mbp.localdomain>
2021-02-03 08:12:23 -0800

    APIs are ready to go

---
+++ illustration_api.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import uvicorn
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+app = FastAPI(
+    title="Labs30-StorySquad-DS-Team A",
+    description="An API for the illustration score",
+    version="0.1",
+    docs_url="/"
+)
+
+# this api is meant to help transfer the illustration similarity scoring model
+# when set up properly, there should be a file called 'transfer_model.h5' in the same scope as this illustration_api file
+# to do this, follow this link: https://colab.research.google.com/drive/1J66ylaqZfZQzCiOmRYHJ4mWt7Jmh7y_B?usp=sharing
+# run all the cells properly, and take the newly downloaded h5 from your Colaboratory workflow to the story-squad-ds-a main folder
+# lastly, uncomment the line below this one
+
+#model = load_model('transfer_model.h5')
+
+if __name__ == "__main__":
+    uvicorn.run(app)
